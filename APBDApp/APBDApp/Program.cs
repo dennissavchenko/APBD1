720a4b159b0d48b00d74eaf218a78c8a9464d85a
s28381 <s28381@pjwstk.edu.pl>
2024-03-17 19:17:59 +0100

    Modification 3

--- APBDApp/APBDApp/Program.cs
+++ APBDApp/APBDApp/Program.cs
@@ -1,8 +1,24 @@
 ﻿Console.WriteLine("Hello, World!");
 
-int n = 10;
+int n = 1;
 
 for (int i = 0; i < n; i++) Console.WriteLine("NUMBER " + i);
 
 if(n >= 10) Console.WriteLine("N is grater or equal than 10");
 else Console.WriteLine("N is smaller than 10");
+
+switch (n)
+{
+    case 1: {
+        Console.WriteLine("Hello");
+        break;
+    }
+    case 2: {
+        Console.WriteLine("Bye");
+        break;
+    }
+    default: {
+        Console.WriteLine("Invalid input!");
+        break;
+    }
+}
