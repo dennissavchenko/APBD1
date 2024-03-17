799a421ed6b882e730a23c9901be3a0f930ced5c
s28381 <s28381@pjwstk.edu.pl>
2024-03-17 19:12:42 +0100

    Modification 2

--- APBDApp/APBDApp/Program.cs
+++ APBDApp/APBDApp/Program.cs
@@ -3,3 +3,6 @@
 int n = 10;
 
 for (int i = 0; i < n; i++) Console.WriteLine("NUMBER " + i);
+
+if(n >= 10) Console.WriteLine("N is grater or equal than 10");
+else Console.WriteLine("N is smaller than 10");
